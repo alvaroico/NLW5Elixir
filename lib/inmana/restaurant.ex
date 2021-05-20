4441db4c9848f78e7e1bd2901bf68d75355e579e
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2021-05-19 21:47:38 -0300

    Inmana.Restaurant.changeset(%{email: "teste@teste.com", name: "siri"})

---
+++ lib/inmana/restaurant.ex
@@ -0,0 +1,19 @@
+defmodule Inmana.Restaurant do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  schema "restaurants" do
+    field :email, :string
+    field :name, :string
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, [:email, :name])
+    |> validate_required([:email, :name])
+  end
+end
